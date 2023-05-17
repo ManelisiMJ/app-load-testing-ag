fc9b53f76a8171193516266f1330c1c3dfc8f8e4
Ncube Manelisi <ncbman005@myuct.ac.za>
2023-05-18 00:16:54 +0200

    First draft of visualization graphs

--- static/js/index.js
+++ static/js/index.js
@@ -2,6 +2,7 @@
 
 import SERVER_IP from './config.js' //Server's IP address
 const testButton = document.getElementById("start-test")
+const visualizeButton = document.getElementById("visualize")
 const url = `${SERVER_IP}/load-test`    //API endpoint in flask server
 let minRequests = 10    //Start number of requests
 let maxRequests = 15    //End number of requests
@@ -49,13 +50,76 @@ function loadTest(endpointUrl){
                 .catch(error => {
                     console.error('Error fetching data:', error);
                 });
-        }    
+        }  
     }
+}
+
+function visualize(){
+    // Extracting the data points
+    const plotData = data.map((obj, index) => ({
+        x: obj.numRequests,
+        y: obj.avgResponseTime,
+    }));
+    
+    // Printing the extracted data for verification
+    console.log(plotData);
+
+    // Setting up the SVG container dimensions
+    const width = 500;
+    const height = 300;
+    const margin = { top: 20, right: 20, bottom: 30, left: 40 };
+    const svgWidth = width + margin.left + margin.right;
+    const svgHeight = height + margin.top + margin.bottom;
+
+    // Creating the SVG container
+    const svg = d3
+    .select("body")
+    .append("svg")
+    .attr("width", svgWidth)
+    .attr("height", svgHeight)
+    .append("g")
+    .attr("transform", `translate(${margin.left},${margin.top})`);
+
+    // Setting up scales for x and y axes
+    const xScale = d3
+    .scaleLinear()
+    .domain([0, d3.max(plotData, (d) => d.x)])
+    .range([0, width]);
+
+    const yScale = d3
+    .scaleLinear()
+    .domain([0, d3.max(plotData, (d) => d.y)])
+    .range([height, 0]);
+
+    // Adding x-axis
+    svg
+    .append("g")
+    .attr("transform", `translate(0, ${height})`)
+    .call(d3.axisBottom(xScale));
+
+    // Adding y-axis
+    svg.append("g").call(d3.axisLeft(yScale));
+
+    // Adding the data points as circles
+    svg
+    .selectAll("circle")
+    .data(plotData)
+    .enter()
+    .append("circle")
+    .attr("cx", (d) => xScale(d.x))
+    .attr("cy", (d) => yScale(d.y))
+    .attr("r", 4)
+    .style("fill", "steelblue");
+
 }
 
 testButton.addEventListener("click", ()=>{
     loadTest(url)
     console.log("data =", data)
+})
+
+visualizeButton.addEventListener("click", ()=>{
+    visualize()
 })
 
 
