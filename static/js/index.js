acaefa88014d4ae08d01dd509f466562b4d2e0dd
Ncube Manelisi <ncbman005@myuct.ac.za>
2023-05-20 13:41:44 +0200

    Added input boxes to get min requests, max requests and interval

--- static/js/index.js
+++ static/js/index.js
@@ -3,54 +3,63 @@
 import SERVER_IP from './config.js' //Server's IP address
 const testButton = document.getElementById("start-test")
 const visualizeButton = document.getElementById("visualize")
+const startBox = document.getElementById("start")
+const endBox = document.getElementById("end")
+const intervalBox = document.getElementById("interval")
 const url = `${SERVER_IP}/load-test`    //API endpoint in flask server
-let minRequests = 10    //Start number of requests
-let maxRequests = 100    //End number of requests
-let interval = 10        //Interval to increment requests
 let data = []
 
 function loadTest(endpointUrl){
-    //Loop through the range [minRequests, maxRequests] incrementing by interval
-    for (let numRequests = minRequests; numRequests<=maxRequests; numRequests+=interval){
-        let responseTimes = [];     //Records the response time for each request sent
-        let bytes = [];             //Stores number of bytes sent from server
+    let minRequests =  Number.parseInt(startBox.value) || 10    //Start number of requests
+    let maxRequests = Number.parseInt(endBox.value) || 100    //End number of requests
+    let interval = Number.parseInt(intervalBox.value) || 10        //Interval to increment requests
+        
+    if (maxRequests < minRequests){
+        alert("Max requests cannot be less than Min requests")
+    }
+    else{
+        //Loop through the range [minRequests, maxRequests] incrementing by interval
+        for (let numRequests = minRequests; numRequests<=maxRequests; numRequests+=interval){
+            let responseTimes = [];     //Records the response time for each request sent
+            let bytes = [];             //Stores number of bytes sent from server
 
-        // Send multiple requests and time them using the PerformanceTiming API
-        for (let i = 0; i < numRequests; i++) {
-            let startTime = performance.now();
-            
-            fetch(endpointUrl)
-                .then(response => response.json())
-                .then(json => {
-                    let endTime = performance.now();
-                    let responseTime = endTime - startTime;     //Calculate response time
-                    responseTimes.push(responseTime);     //Add response time to array
+            // Send multiple requests and time them using the PerformanceTiming API
+            for (let i = 0; i < numRequests; i++) {
+                let startTime = performance.now();
+                
+                fetch(endpointUrl)
+                    .then(response => response.json())
+                    .then(json => {
+                        let endTime = performance.now();
+                        let responseTime = endTime - startTime;     //Calculate response time
+                        responseTimes.push(responseTime);     //Add response time to array
 
-                    let byteCount = new TextEncoder().encode(JSON.stringify(json)).length;  //Bytes sent by server
-                    bytes.push(byteCount);
+                        let byteCount = new TextEncoder().encode(JSON.stringify(json)).length;  //Bytes sent by server
+                        bytes.push(byteCount);
 
-                    // If all requests have completed, calculate statistics for the No. of requests
-                    if (responseTimes.length === numRequests) {
-                        let avgResponseTime = responseTimes.reduce((a, b) => a + b, 0) / numRequests;
-                        let maxResponseTime = Math.max(...responseTimes);
-                        let minResponseTime = Math.min(...responseTimes);
-                        let totalbytes = bytes.reduce((a, b) => a + b, 0);
-                        
-                        //Store statistics in dataPoint object
-                        let dataPoint = {
-                            "numRequests": numRequests,
-                            "avgResponseTime": avgResponseTime,
-                            "maxResponseTime": maxResponseTime,
-                            "minResponseTime": minResponseTime,
-                            "bytesSent": totalbytes
+                        // If all requests have completed, calculate statistics for the No. of requests
+                        if (responseTimes.length === numRequests) {
+                            let avgResponseTime = responseTimes.reduce((a, b) => a + b, 0) / numRequests;
+                            let maxResponseTime = Math.max(...responseTimes);
+                            let minResponseTime = Math.min(...responseTimes);
+                            let totalbytes = bytes.reduce((a, b) => a + b, 0);
+                            
+                            //Store statistics in dataPoint object
+                            let dataPoint = {
+                                "numRequests": numRequests,
+                                "avgResponseTime": avgResponseTime,
+                                "maxResponseTime": maxResponseTime,
+                                "minResponseTime": minResponseTime,
+                                "bytesSent": totalbytes
+                            }
+                            data.push(dataPoint)    //Add dataPoint to data array
                         }
-                        data.push(dataPoint)    //Add dataPoint to data array
-                    }
-                })
-                .catch(error => {
-                    console.error('Error fetching data:', error);
-                });
-        }  
+                    })
+                    .catch(error => {
+                        console.error('Error fetching data:', error);
+                    });
+            }  
+        }
     }
 }
 
